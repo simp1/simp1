c336361235c157b7a6cf3c87f53dddd480099e36
simp1 <37863169+simp1@users.noreply.github.com>
2018-05-07 15:04:33 +0200

    qr-code für db rdy

---
+++ WebContent/Includes/QRCode/insertqr.inc.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	header('Access-Control-Allow-Origin:*');
+	header('Access-Control-Allow-Methods: GET');
+	
+	include '../functions.inc.php';
+	include '../config.inc.php';
+	$parts  = $_POST['datastring'];
+	$teile = explode(";;",$parts);
+	$token=$teile[3];
+	$token_login=$teile[4];
+	$username=$teile[2];
+	$werkzeugID=$teile[0];
+	$qr=$teile[1];
+	$qr = str_replace(' ','+',$qr);
+
+	if(checktoken($token,$token_login,$username)){
+		$erg = status($username);
+		if($erg>=1){
+			$sql="INSERT INTO qrcode (werkzeugID,svg) VALUES (?,?)";
+			$stmt = $con->prepare($sql);
+			$stmt->bind_param('is', $werkzeugID, $qr);
+			$stmt->execute();
+			echo "success";
+			exit();
+		}else{
+			echo "rights";
+			exit();
+		}
+	}else{
+		echo "token";
+		exit();
+	}
+	
+?>
