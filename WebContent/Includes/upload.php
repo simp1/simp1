5912dc9cbdff8d7b67e12df00e0c1eb0b4523aa1
simp1 <37863169+simp1@users.noreply.github.com>
2018-05-31 11:16:55 +0200

    Add files via upload

--- WebContent/Includes/upload.php
+++ WebContent/Includes/upload.php
@@ -7,11 +7,16 @@ $idzuge=$_POST['idzuge'];
 $target_dir = "../uploads/pdf/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
+$werkid;
 $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
+$sql="SELECT * FROM stammdaten WHERE werkzeug_nummer='".$werkzeugID."' AND entfernt=0";
+$statemt = getsql($sql);
+while($ausgabe = $statemt->fetch_object()){
+	$werkid = $ausgabe->werkzeugID;
+}
 // Check if file already exists
 if (file_exists($target_file)) {
-	header("Location: http://localhost/workspace/swimp/WebContent/test.html?&exist");
+	header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&exist");
     echo "Sorry, file already exists.";
     
     exit();
@@ -20,21 +25,21 @@ if (file_exists($target_file)) {
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 50000000) {
     echo "Sorry, your file is too large.";
-    header("Location: http://localhost/workspace/swimp/WebContent/test.html?&large");
+    header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&large");
     exit();
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($fileType != "pdf") {
 			echo "Sorry, only PDF files are allowed.";
-			header("Location: http://localhost/workspace/swimp/WebContent/test.html?&format");
+			header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&format");
 			exit();
 			$uploadOk = 0;
 		}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-    header("Location: http://localhost/workspace/swimp/WebContent/test.html?&nosuccess");
+    header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&nosuccess");
     exit();
     
 // if everything is ok, try to upload file
@@ -76,16 +81,16 @@ if ($uploadOk == 0) {
     		$stmt->execute();
     	}else{
     		$stmt = $con->prepare($sql);
-    		$stmt->bind_param('issss', $werkzeugID, $teile[1], $url, $teile[0], $zuordnung);
+    		$stmt->bind_param('issss', $werkid, $teile[1], $url, $teile[0], $zuordnung);
     		$stmt->execute();
     	}
     	
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        header("Location: http://localhost/workspace/swimp/WebContent/test.html?&success");
+        header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&success");
         exit();
     } else {
         echo "Sorry, there was an error uploading your file.";
-        header("Location: http://localhost/workspace/swimp/WebContent/test.html?&nosuccess");
+        header("Location: http://localhost/workspace/swimp/WebContent/werkzeugverwaltung.html?&werkzeugID=".$werkzeugID."&nosuccess");
         exit();
     }
 }
